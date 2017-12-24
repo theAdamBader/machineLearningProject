1568e963bf72e938dff63c0c50d3629539a2e11a
Adam Bader <abade011@gold.ac.uk>
2017-12-24 23:04:23 +0000

    level one

---
+++ fingerBang/Assets/Scripts/Cycle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cycle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+	}
+}

e22b8e736711be0f832c21e5cd30c29ae40b024f
Adam Bader <abade011@gold.ac.uk>
2017-12-27 12:25:18 +0000

    enemey health

---
+++ fingerBang/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int currentHealth = 10;
+
+	public void damageTaken (int amount){
+		currentHealth -= amount;
+
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}

ae162df6bfe02ac22c5efffbc305522ff58b46bd
Adam Bader <abade011@gold.ac.uk>
2018-01-01 21:31:37 +0000

    added simple menu

---
+++ fingerBang/Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,22 @@
+﻿/*
+REFERENCE
+	Menu UI: https://youtu.be/FrJogRBSzFo
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour {
+
+	public void StartButton(string levelOne){
+
+		SceneManager.LoadScene (levelOne);
+	}
+
+	public void ExitButton(string exitGame){
+
+		Application.Quit ();
+	}
+}

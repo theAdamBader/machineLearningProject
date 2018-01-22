83d72712e26deb8e5e585f5c8cd9d260b56c37c0
Adam Bader <abade011@gold.ac.uk>
2018-01-22 12:21:12 +0000

    bullet functions on the OSC Receiver script

--- fingerBang/Assets/Scripts/OSCReceiver.js
+++ fingerBang/Assets/Scripts/OSCReceiver.js
@@ -6,7 +6,20 @@ public var ListenerPort : int = 12000;
 public var controller : Transform; 
 private var handler : Osc;
 private var sig1 : int = 0;
-private var sig2 : int = 0;
+private var lastFeat: float = 1; 
+
+public var bulletPrefab: GameObject;
+public var rightBulletSpawn: Transform;
+public var leftBulletSpawn: Transform;
+
+public var fireRate: float = 0.4;
+private var nextShot: float = 0;
+
+public var maxAmmo: int = 10;
+private var currentAmmo: int;
+public var reloadSpeed: float = 1;
+
+//public var rightAmmoCount: guiText;
 
 
 public function Start ()
@@ -22,14 +35,40 @@ public function Start ()
 
 	//Tell Unity to call function Example1 when message /wek/outputs arrives
 	handler.SetAddressHandler("/wek/outputs", Example1);
+
+	//Starts with current ammo
+	currentAmmo = maxAmmo;
+
 }
 Debug.Log("OSC Running");
 
 //Use the values from OSC to do stuff
 function Update () {
-if (Input.GetKeyDown ("1")){Debug.Log( "1 key was pressed." );}
+	
+		if (sig1 == 1 && Time.time > nextShot) {
+
+			//If current ammo is more than zero then fire weapon
+			if (currentAmmo > 0) {
+				nextShot = Time.time + fireRate;
+				RightFire();
+			}
+		}
+
+		 if (sig1 == 2){
+			Reload();
+		}
+
+		if (sig1 == 3 && Time.time > nextShot) {
+
+			//If current ammo is more than zero then fire weapon
+			if (currentAmmo > 0) {
+				nextShot = Time.time + fireRate;
+				LeftFire();
+			}
+		}
 
 }
+
 //This is called when /wek/outputs arrives, since this is what's specified in Start()
 public function Example1(oscMessage : OscMessage) : void
 {	
@@ -37,6 +76,52 @@ public function Example1(oscMessage : OscMessage) : void
 	Debug.Log("Called Example One > " + Osc.OscMessageToString(oscMessage));
 	Debug.Log("Message Values > " + oscMessage.Values[0] + " " + oscMessage.Values[1]);
 	sig1 = oscMessage.Values[0];
-	sig2 = oscMessage.Values[1];
-	
+
 } 
+
+function RightFire(){
+
+var bullet: GameObject;
+bullet = Instantiate (bulletPrefab, rightBulletSpawn.transform.position, rightBulletSpawn.transform.rotation) as GameObject;
+
+bullet.GetComponent(Rigidbody).velocity = bullet.transform.forward * 6;
+
+
+	// Destroy the bullet after 2 seconds
+	Destroy(bullet, 2.0f);
+
+	//Lose one bullet everytime the weapon is shot
+	currentAmmo--;
+
+}
+
+function LeftFire(){
+
+var bullet: GameObject;
+bullet = Instantiate (bulletPrefab, leftBulletSpawn.transform.position, leftBulletSpawn.transform.rotation) as GameObject;
+
+bullet.GetComponent(Rigidbody).velocity = bullet.transform.forward * 6;
+
+
+	// Destroy the bullet after 2 seconds
+	Destroy(bullet, 2.0f);
+
+	//Lose one bullet everytime the weapon is shot
+	currentAmmo--;
+
+}
+
+	function Reload(){
+		
+		Debug.Log ("Reloading...");
+
+		//Get accessor to a new wait with a reload speed
+		yield new WaitForSeconds (reloadSpeed);
+
+		//When reloading, current ammo grabs the max ammo
+		currentAmmo = maxAmmo;
+
+	
+	}
+
+
--- fingerBang/Assets/Scripts/bullet.js
+++ fingerBang/Assets/Scripts/bullet.js
@@ -1,4 +1,12 @@
-﻿#pragma strict
+﻿
+//You can set these variables in the scene because they are public 
+public var RemoteIP : String = "127.0.0.1";
+public var SendToPort : int = 6448;
+public var ListenerPort : int = 12000;
+public var controller : Transform; 
+private var handler : Osc;
+private var sig1 : int = 0;
+protected var lastFeat: float = 1; 
 
 public var bulletPrefab: GameObject;
 public var bulletSpawn: Transform;
@@ -10,13 +18,47 @@ public var maxAmmo: int = 10;
 private var currentAmmo: int;
 
 
-function Start () {
+public function Start ()
+{
+	//Initializes on start up to listen for messages
+	//make sure this game object has both UDPPackIO and OSC script attached
+	
+	var udp : UDPPacketIO = GetComponent("UDPPacketIO");
+	udp.init(RemoteIP, SendToPort, ListenerPort);
+	handler = GetComponent("Osc");
+	handler.init(udp);
+			
+
+	//Tell Unity to call function Example1 when message /wek/outputs arrives
+	handler.SetAddressHandler("/wek/outputs", Example1);
+
 	currentAmmo = maxAmmo;
 }
+Debug.Log("OSC Running");
 
+//Use the values from OSC to do stuff
 function Update () {
-	
+	if (lastFeat != sig1){
+		if (sig1 == 1) {
+			Debug.Log ("1 was pressed");
+			Fire();
+		}
+	}
+
+	lastFeat = sig1;
 }
+
+//This is called when /wek/outputs arrives, since this is what's specified in Start()
+public function Example1(oscMessage : OscMessage) : void
+{	
+	
+	Debug.Log("Called Example One > " + Osc.OscMessageToString(oscMessage));
+	Debug.Log("Message Values > " + oscMessage.Values[0] + " " + oscMessage.Values[1]);
+	sig1 = oscMessage.Values[0];
+//	sig2 = oscMessage.Values[1];
+
+
+} 
 
 function Fire(){
 
